21ce67814cfde805bcdf26850dbb4742beab66d4
d1slike <merser1q@gmail.com>
2022-09-27 16:43:17 +0300

    MSP-1060 update remote config

--- conf/remoteConfig.go
+++
@@ -1,9 +0,0 @@
-package conf
-
-import (
-	"github.com/integration-system/isp-lib/v2/structure"
-)
-
-type RemoteConfig struct {
-	Metrics structure.MetricConfiguration `schema:"Настройка метрик"`
-}
